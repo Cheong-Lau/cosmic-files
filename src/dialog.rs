ce624bdfddac37224afd2757828cae0414303941
Cheong Lau <234708519+Cheong-Lau@users.noreply.github.com>
2025-12-03 16:40:05 +1000

    improv: improve error handling
    This changes the `Mounter` trait methods and other functions that return
    `Box<dyn Error>` or `String` to now return an `anyhow::Error`.
    
    The `large_image` instead has its error handling simplified with a new
    simple custom error type.

--- src/dialog.rs
+++ src/dialog.rs
@@ -1290,7 +1290,7 @@ impl Application for App {
         {
             return mounter
                 .mount(data.1.clone())
-                .map(|()| cosmic::action::none());
+                .map(|_res| cosmic::action::none());
         }
         Task::none()
     }
@@ -1862,7 +1862,7 @@ impl Application for App {
                                     match glob::Pattern::new(value) {
                                         Ok(glob) => parsed_globs.push(glob),
                                         Err(err) => {
-                                            log::warn!("failed to parse glob {value:?}: {err}");
+                                            log::warn!("failed to parse glob {value}: {err}");
                                         }
                                     }
                                 }
@@ -2072,7 +2072,7 @@ impl Application for App {
                                                 Ok(()) => {}
                                                 Err(err) => {
                                                     log::warn!(
-                                                        "failed to send notify events: {err:?}"
+                                                        "failed to send notify events: {err}"
                                                     );
                                                 }
                                             }
@@ -2101,7 +2101,7 @@ impl Application for App {
                             }
                         }
                         Err(err) => {
-                            log::warn!("failed to create file watcher: {err:?}");
+                            log::warn!("failed to create file watcher: {err}");
                         }
                     }
 
